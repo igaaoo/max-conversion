ec5ed90a3df649c6290392232d79017e5c5a71e6
Igor Neves <igorneves0404@gmail.com>
2022-02-04 18:18:54 -0300

    Create Routes

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,24 +1,30 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 
-import { Binario } from './pages/Binario'
+import { Binary } from './pages/Binary'
 import { Home } from './pages/Home'
 import { Morse } from './pages/Morse'
+
+import '../src/App.css'
 
 // Para criar rotas: Importar as dependências da primeira linha, importar os components que serão as rotas
 // Criar a estrutura <Router><Routes><Route path="/endereço" element={<Component/>}></Routes></Router>
 
 export function AppRoutes() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Navigate to="/home" replace />} />
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Navigate to="/home" replace />} />
 
-        <Route path="/home" element={<Home />} />
+          <Route path="/home" element={<Home />} />
 
-        <Route path="/binario" element={<Binario />} />
+          <Route path="/binary" element={<Binary />} />
 
-        <Route path="/morse" element={<Morse />} />
-      </Routes>
-    </Router>
+          <Route path="/morse" element={<Morse />} />
+
+          <Route path="*" element={<Navigate to="/home" replace />} />
+        </Routes>
+      </Router>
+    </div>
   )
 }
